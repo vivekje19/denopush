09c4debfb9bba318855731d10df76fc1b6e58cc9
Vivek Ranjan Pandey <vivekranjan.varanasi@gmail.com>
2021-08-04 00:33:09 +0530

    third commit

--- routes.ts
+++ routes.ts
@@ -8,9 +8,9 @@ import {
 } from "./clientAuthenticaion.ts";
 export const router = new Router()
   .get("/", (ctx: Context): void => {
-    ctx.response.body = { data: "Hello worlds!" };
+    ctx.response.body = { data: "This is push notification API!" };
   })
-  .get("/vapidKey", (ctx: Context): void => {
+  .get("/vapidKey", clientValidate, (ctx: Context): void => {
     console.log(ctx.request);
     ctx.response.body = { key: config()["VAPID_PUBLIC_KEY"] };
   })
